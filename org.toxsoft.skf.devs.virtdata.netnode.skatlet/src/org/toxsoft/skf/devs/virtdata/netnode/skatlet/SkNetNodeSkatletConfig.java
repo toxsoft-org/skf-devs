4c846f8961ca67362ebded7142c0d0851410cdf0
mvk <kovach.mike@gmail.com>
2025-03-16 20:59:06 +0700

    SkNetNodeSkatletConfig. minor changes

--- org.toxsoft.skf.devs.virtdata.netnode.skatlet/src/org/toxsoft/skf/devs/virtdata/netnode/skatlet/SkNetNodeSkatletConfig.java
+++ org.toxsoft.skf.devs.virtdata.netnode.skatlet/src/org/toxsoft/skf/devs/virtdata/netnode/skatlet/SkNetNodeSkatletConfig.java
@@ -31,8 +31,17 @@ public final class SkNetNodeSkatletConfig {
    * Пример: <code>
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.health.output0=@Gwid[sk.Server[valcom.server.main]$rtdata(health)]
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.health.inputs0=@GwidList[sk.Server[valcom.server.local1]$rtdata(health),sk.Server[valcom.server.local2]$rtdata(health)]]
-   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output0=@Gwid[sk.Server[valcom.server.local1]$rtdata(online)]
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input0=@Gwid[sk.Server[valcom.server.local1]$rtdata(health)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input1=@Gwid[val.Registrator[owen_VK1]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input2=@Gwid[val.Registrator[owen_VK2]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input3=@Gwid[val.Registrator[owen_VK3]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input4=@Gwid[val.Registrator[owen_VK4]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input5=@Gwid[val.Registrator[owen_VK5]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output1=@Gwid[val.Registrator[owen_VK1]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output2=@Gwid[val.Registrator[owen_VK2]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output3=@Gwid[val.Registrator[owen_VK3]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output4=@Gwid[val.Registrator[owen_VK4]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output5=@Gwid[val.Registrator[owen_VK5]$rtdata(online)]
    * </code>
    */
   public static final String NETNODE_HEALTH_OUTPUT_PREFIX = SYBSYSTEM_ID_PREFIX + ".health.output";
@@ -51,8 +60,17 @@ public final class SkNetNodeSkatletConfig {
    * Пример: <code>
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.health.output0=@Gwid[sk.Server[valcom.server.main]$rtdata(health)]
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.health.inputs0=@GwidList[sk.Server[valcom.server.local1]$rtdata(health),sk.Server[valcom.server.local2]$rtdata(health)]]
-   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output0=@Gwid[sk.Server[valcom.server.local1]$rtdata(online)]
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input0=@Gwid[sk.Server[valcom.server.local1]$rtdata(health)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input1=@Gwid[val.Registrator[owen_VK1]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input2=@Gwid[val.Registrator[owen_VK2]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input3=@Gwid[val.Registrator[owen_VK3]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input4=@Gwid[val.Registrator[owen_VK4]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input5=@Gwid[val.Registrator[owen_VK5]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output1=@Gwid[val.Registrator[owen_VK1]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output2=@Gwid[val.Registrator[owen_VK2]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output3=@Gwid[val.Registrator[owen_VK3]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output4=@Gwid[val.Registrator[owen_VK4]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output5=@Gwid[val.Registrator[owen_VK5]$rtdata(online)]
    * </code>
    */
   public static final String NETNODE_HEALTH_INPUTS_PREFIX = SYBSYSTEM_ID_PREFIX + ".health.inputs";
@@ -73,8 +91,17 @@ public final class SkNetNodeSkatletConfig {
    * Пример: <code>
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.health.output0=@Gwid[sk.Server[valcom.server.main]$rtdata(health)]
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.health.inputs0=@GwidList[sk.Server[valcom.server.local1]$rtdata(health),sk.Server[valcom.server.local2]$rtdata(health)]]
-   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output0=@Gwid[sk.Server[valcom.server.local1]$rtdata(online)]
    * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input0=@Gwid[sk.Server[valcom.server.local1]$rtdata(health)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input1=@Gwid[val.Registrator[owen_VK1]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input2=@Gwid[val.Registrator[owen_VK2]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input3=@Gwid[val.Registrator[owen_VK3]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input4=@Gwid[val.Registrator[owen_VK4]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.input5=@Gwid[val.Registrator[owen_VK5]$link(lnkAnalogChannels)]]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output1=@Gwid[val.Registrator[owen_VK1]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output2=@Gwid[val.Registrator[owen_VK2]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output3=@Gwid[val.Registrator[owen_VK3]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output4=@Gwid[val.Registrator[owen_VK4]$rtdata(online)]
+   * -Dorg.toxsoft.uskat.skatlets.virtdata.netnode.online.output5=@Gwid[val.Registrator[owen_VK5]$rtdata(online)]
    * </code>
    */
   public static final String NETNODE_ONLINE_OUTPUT_PREFIX = SYBSYSTEM_ID_PREFIX + ".online.output";
