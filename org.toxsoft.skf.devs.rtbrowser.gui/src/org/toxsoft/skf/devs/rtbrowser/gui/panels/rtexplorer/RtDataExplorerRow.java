e0f9d4b7c4868648b13a2b1ef7446f1c6d87b849
Max <max@max.max>
2025-07-31 10:04:12 +0300

    Вместо id - nmName для класса и бъекта

--- org.toxsoft.skf.devs.rtbrowser.gui/src/org/toxsoft/skf/devs/rtbrowser/gui/panels/rtexplorer/RtDataExplorerRow.java
+++ org.toxsoft.skf.devs.rtbrowser.gui/src/org/toxsoft/skf/devs/rtbrowser/gui/panels/rtexplorer/RtDataExplorerRow.java
@@ -3,8 +3,15 @@ package org.toxsoft.skf.devs.rtbrowser.gui.panels.rtexplorer;
 import org.toxsoft.core.tslib.gw.skid.*;
 import org.toxsoft.skf.devs.rtbrowser.gui.panels.rtDatas.*;
 import org.toxsoft.uskat.core.*;
+import org.toxsoft.uskat.core.api.objserv.*;
+import org.toxsoft.uskat.core.api.sysdescr.*;
 import org.toxsoft.uskat.core.api.sysdescr.dto.*;
 
+/**
+ * Модель (в парадигме MVC) одного ряда данных компонента {@link RtDataExplorer}
+ *
+ * @author max
+ */
 public class RtDataExplorerRow
     extends RtDataBrowserRow {
 
@@ -12,10 +19,27 @@ public class RtDataExplorerRow
 
   private IDtoRtdataInfo rtDataInfo;
 
-  public RtDataExplorerRow( ISkCoreApi aCoreApi, ISkidList aSkidList, IDtoRtdataInfo aRtdataInfo ) {
-    super( aCoreApi, aSkidList, aRtdataInfo );
-    skid = aSkidList.first();
+  private String objName;
+
+  private String className;
+
+  /**
+   * Конструктор
+   *
+   * @param aCoreApi server's API
+   * @param aSkid {@link Skid} данное которого отображается
+   * @param aRtdataInfo описание данного
+   */
+  public RtDataExplorerRow( ISkCoreApi aCoreApi, Skid aSkid, IDtoRtdataInfo aRtdataInfo ) {
+    super( aCoreApi, new SkidList( aSkid ), aRtdataInfo );
+    skid = aSkid;
     rtDataInfo = aRtdataInfo;
+
+    ISkObject obj = coreApi.objService().find( skid );
+    objName = obj != null ? obj.nmName() + " (" + obj.description() + ")" : skid.strid();
+
+    ISkClassInfo classInfo = coreApi.sysdescr().findClassInfo( skid.classId() );
+    className = classInfo != null ? classInfo.nmName() : skid.classId();
   }
 
   Skid getSkid() {
@@ -28,6 +52,14 @@ public class RtDataExplorerRow
 
   String getObjId() {
     return skid.strid();
+  }
+
+  String getObjName() {
+    return objName;
+  }
+
+  String getClassName() {
+    return className;
   }
 
   IDtoRtdataInfo dataInfo() {
