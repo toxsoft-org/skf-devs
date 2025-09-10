39a7544ebf50904780f4858e7a552ca1bcb1e5f8
Dmitry Egorov <egorov.dmitry.alex@gmail.com>
2025-09-10 10:08:27 +0300

    removeSettings() added

--- org.toxsoft.skf.devs.rtbrowser.gui/src/org/toxsoft/skf/devs/rtbrowser/gui/panels/rtexplorer/ultils/RtDataExplorerUtils.java
+++ org.toxsoft.skf.devs.rtbrowser.gui/src/org/toxsoft/skf/devs/rtbrowser/gui/panels/rtexplorer/ultils/RtDataExplorerUtils.java
@@ -5,8 +5,6 @@ import static org.toxsoft.uskat.core.ISkHardConstants.*;
 
 import org.eclipse.jface.window.*;
 import org.eclipse.swt.graphics.*;
-import org.toxsoft.core.tsgui.bricks.ctx.*;
-import org.toxsoft.core.tsgui.bricks.ctx.impl.*;
 import org.toxsoft.core.tslib.av.impl.*;
 import org.toxsoft.core.tslib.bricks.strid.idgen.*;
 import org.toxsoft.core.tslib.gw.gwid.*;
@@ -52,40 +50,6 @@ public class RtDataExplorerUtils {
       IDtoFullObject dtoSetting = makeSettingsPackDto( aCoreApi, settingsName, gwidList, location, size );
       DtoFullObject.defineFullObject( aCoreApi, dtoSetting );
     }
-  }
-
-  /**
-   * Remove selected settings.
-   * <p>
-   *
-   * @param aCoreApi - server API
-   * @param aContext - app context
-   */
-  public static void removeSettings( ISkCoreApi aCoreApi, TsGuiContext aContext ) {
-    // get setting pack id
-    IRtDataExplorerSettings selPack = RtDataExplorerSettingSelector.selectSettingsPack( null, aContext );
-    if( selPack != null ) {
-      aCoreApi.objService().removeObject( new Skid( IRtDataExplorerSettings.CLASS_ID, selPack.strid() ) );
-    }
-  }
-
-  /**
-   * Load selected settings.
-   * <p>
-   *
-   * @param aCoreApi - server API
-   * @param aContext - app context
-   * @return {@link RtDataExplorerSettings} - selected settings pack
-   */
-  public static IRtDataExplorerSettings selectSettings( ISkCoreApi aCoreApi, ITsGuiContext aContext ) {
-    IRtDataExplorerSettings retVal = null;
-    // get setting pack id
-    IRtDataExplorerSettings selPack = RtDataExplorerSettingSelector.selectSettingsPack( null, aContext );
-    if( selPack != null ) {
-      // retVal = aCoreApi.objService().find( new Skid( IRtDataExplorerSettings.CLASS_ID, selPack.strid() ) );
-      retVal = selPack;
-    }
-    return retVal;
   }
 
   private static IDtoFullObject makeSettingsPackDto( ISkCoreApi aCoreApi, String aName, GwidList aRtGwids,
